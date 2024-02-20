2bf82085232ba0d98333edafe7d52818eec9ffda
CaptainBeagle <118069099+CaptainBeagle@users.noreply.github.com>
2024-02-20 13:26:14 -0600

    Add files via upload

---
+++ Python_Web_Server_Spring_2024-main/server.py
@@ -0,0 +1,187 @@
+from asyncio.windows_events import NULL
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import json
+from socketserver import BaseServer
+
+f = open("test.json")
+print(f)
+users = json.load(f)
+routes = {
+    "/": "Homepage",
+    "/welcome": "Hello World!",
+    "/about": "copyright reserved by Blade",
+    "/index": open("index.html").read(),
+    "/login": open("UserHome.html").read(),
+    "/accept": open("Accept.html").read(),
+    "/register": open("Register.html").read(),
+    "/app": open("App.html").read(),
+}
+
+error = False
+error_message = ""
+class TestServer(BaseHTTPRequestHandler):
+    # def is the keyword for function
+    # function in Java
+    # this is the entry server waiting for http GET request
+    # we are writing an http get response
+    def do_GET(self):
+        global error
+        global error_message
+        
+        if error:
+            error = False
+        else:
+            error_message = ""
+        for route in routes:
+            if route == self.path or (self.path.startswith(route) and route != '/'):
+                self.send_response(200) # OK
+                self.send_header("Content-type", "text/html")
+                self.end_headers()
+                self.wfile.write(bytes(routes[route].replace("###ERROR###", error_message), "UTF-8"))
+                break
+        if self.path.startswith("/api/"):
+            if self.path.endswith("/login"):
+                self.send_response(200)
+                self.send_header("content-type", "application/json")
+                self.end_headers()
+                result = {
+                    "status": 200,
+                    "html": open("login-form.html").read()
+                }
+                self.wfile.write(bytes(json.dumps(result), "UTF-8"))
+            elif self.path.endswith("/register"):
+                self.send_response(200)
+                self.send_header("content-type", "application/json")
+                self.end_headers()
+                result = {
+                    "status": 200,
+                    "html": open("Register.html") #try using separate file with form only
+                }
+                self.wfile.write(bytes(json.dumps(result), "UTF-8"))    
+    
+    
+    def do_POST(self):
+        global error
+        global error_message
+        content_length = int(self.headers['Content-Length'])
+        post_data = self.rfile.read(content_length).decode("utf-8")
+        pairs = post_data.split('&')
+        form = {}
+        for pair in pairs:
+            kvp = pair.split('=')
+            form[kvp[0]] = kvp[1]
+        match = False
+        foundUser = False
+        for key in users:
+            user = users[key]
+            if form["username"] == user["username"]:
+                foundUser = True
+            if form["username"] == user["username"] and form["password"] == user["password"]:
+                match = True
+                break
+        if self.path == "/accept":
+            if match:
+                self.send_response(200)
+                self.send_header("Content-type", "text/html")
+                self.end_headers()
+                self.wfile.write(bytes(routes[self.path].replace("###USER###", form["username"]), "UTF-8"))
+            else:
+                error = True
+                error_message = "Incorrect Username and password"
+                print(error_message)
+                self.send_response(301)
+                self.send_header('Location','/login')
+                self.end_headers()
+        elif self.path == "/register":
+            # same username, reject
+            if foundUser:
+                error = True
+                error_message = "Username is already in the system."
+                self.send_response(301)
+                self.send_header('Location','/register')
+                self.end_headers()
+            # password and password2 doesn't match, reject
+            elif form["password"] != form["password2"]:
+                error = True
+                error_message = "Passwords mismatch."
+                self.send_response(301)
+                self.send_header('Location','/register')
+                self.end_headers()
+            # store the username and password, and show the user home, accept
+            else:
+                error = False
+                self.send_response(200)
+                self.send_header("Content-type", "text/html")
+                self.end_headers()
+                users[str(len(users))] = {
+                    "username": form["username"],
+                    "password": form["password"]
+                }
+                with open("users.json", "w") as outfile:
+                    json.dump(users, outfile)
+                self.wfile.write(bytes(routes["/accept"].replace("###USER###", form["username"]), "UTF-8"))
+        elif self.path == "/api/login":
+            self.send_response(200)
+            self.send_header("content-type", "application/json")
+            self.end_headers()
+            if match:
+                result = {
+                    "answer": 200,
+                    "username": form["username"]
+                }
+                self.wfile.write(bytes(json.dumps(result), "UTF-8"))
+            else:
+                result = {
+                    "answer": 400,
+                    "error-message": "Incorrect username and password."
+                }
+                self.wfile.write(bytes(json.dumps(result), "UTF-8"))
+        elif self.path == "/api/register":
+            # same username, reject
+            if foundUser:
+                error = True
+                error_message = "Username is already in the system."
+                self.send_response(301)
+                self.send_header('Location','/register')
+                self.end_headers()
+            # password and password2 doesn't match, reject
+            elif form["password"] != form["password2"]:
+                error = True
+                error_message = "Passwords mismatch."
+                self.send_response(301)
+                self.send_header('Location','/register')
+                self.end_headers()
+            # store the username and password, and show the user home, accept
+            else:
+                error = False
+                self.send_response(200)
+                self.send_header("Content-type", "text/html")
+                self.end_headers()
+                users[str(len(users))] = {
+                    "username": form["username"],
+                    "password": form["password"]
+                }
+                with open("users.json", "w") as outfile:
+                    json.dump(users, outfile)
+                self.wfile.write(bytes(routes["/accept"].replace("###USER###", form["username"]), "UTF-8"))
+            #Does this really work?
+
+
+    def redirect(self):
+        self.send_response(301)
+        self.send_header('Location','/login')
+        self.end_headers()
+        
+        
+        
+# this is like run method
+if __name__ == "__main__":
+    webServer = HTTPServer(("localhost", 8080), TestServer)
+    
+    try:
+        webServer.serve_forever()
+    except KeyboardInterrupt:
+        pass
+    
+    webServer.server_close()
+    print("Server stopped.")
